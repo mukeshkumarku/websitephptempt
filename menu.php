8d6d74a392400ef586b98b8f6cdbfba6b0b7ef4b
[mukeshcode] <[mukesh.ssvm@gmail.com]>
2019-10-06 11:05:33 +0530

    it comes from awebsitehtmltophp (localsystem)

---
+++ menu.php
@@ -0,0 +1,45 @@
+<?php
+include 'header.php';
+
+
+?>
+
+<div class="topnav">
+  <a class="active" href="index.php">Home</a>
+  <a href="about.php">About</a>
+  <a href="contact.php">Contact</a>
+  <a href="agent.php">Agent</a>
+  <a href="blog.php">Blog</a>
+  <a href="properties.php">Properties</a>
+  <a href="services.php">Services</a>
+</div>
+<style>
+body {
+  margin: 0;
+  font-family: Arial, Helvetica, sans-serif;
+}
+
+.topnav {
+  overflow: hidden;
+  background-color: #ff8000;
+}
+
+.topnav a {
+  float: left;
+  color: #f2f2f2;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+  font-size: 17px;
+}
+
+.topnav a:hover {
+  background-color: #ddd;
+  color: black;
+}
+
+.topnav a.active {
+  background-color: #4CAF50;
+  color: white;
+}
+</style>
